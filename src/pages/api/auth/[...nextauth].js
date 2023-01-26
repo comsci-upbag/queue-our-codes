696606e6743243bd3715c2f4f6f40c50ca3f9e74
Brymer <meneses.brymer@gmail.com>
2023-01-27 00:13:54 +0800

    add `secret` environment variable

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,13 +2,15 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
-    providers: [
-        // OAuth authentication providers...
-        GoogleProvider({
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET
-        }),
-    ]
+  providers: [
+    // OAuth authentication providers...
+    GoogleProvider({
+        clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET
+    }),
+  ],
+  // https://github.com/nextauthjs/next-auth/issues/3245
+  secret: process.env.SECRET,
 }
 
 export default NextAuth(authOptions)
