a19f1403517a86e3f83b380497a9b1a5831b8586
Daniel David Bador <danieldavidbador@yahoo.com>
2023-01-26 23:17:12 +0800

    minox fixes on HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,8 @@
 import { useSession, signOut } from "next-auth/react"
 import styles from "@/styles/Home.module.css"
 import Image from "next/image"
-import logoutImage from "../images/logout.png"
+import logoutImage from "../images/logout.svg"
+import enterImage from "../images/enter.svg"
 
 export default function HomePage() {
   const { data: session } = useSession()
@@ -10,7 +11,7 @@ export default function HomePage() {
   const userName = session!.user.name;
   const userImage = session!.user.image.replaceAll("s96-c", "s192-c");
 
-  var clueNum = 4;
+  var clueNum = 6;
 
   return (
     <>
@@ -32,14 +33,18 @@ export default function HomePage() {
             I get bigger the more is taken away
           </span>
         </div>
+        <div className={styles.HomeContainer} id={styles.InputContainer}>
+          <input id={styles.InputField} type="text" placeholder="Answer" />
+          <Image id={styles.SubmitButtonImage} src={enterImage} alt="Picture of the user" onClick={() => console.log("button click")} />
+        </div>
         <div className={styles.ProgressBar}>
-          <div className={styles.Progress} style={{ width: (clueNum / 10) * 100 + "%" }}></div >
+          <div className={styles.Progress} style={{ width: "calc(" + (clueNum / 10) * 100 + "% - 4px)" }}></div >
         </div>
         <div className={styles.Footer}>COMSCI@UP.BAG 2023</div>
       </div>
 
       <div className={styles.cover}>
-        <div className={styles.circle}/>
+        <div className={styles.circle} />
       </div>
 
     </>
