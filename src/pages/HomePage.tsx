7044f8cdf3142e77b424bbc675005dc9a3667391
Daniel David Bador <danieldavidbador@yahoo.com>
2023-01-26 22:32:36 +0800

    added logout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 
-import { useSession } from "next-auth/react"
+import { useSession, signOut } from "next-auth/react"
 import styles from "@/styles/Home.module.css"
 import Image from "next/image"
-import profilePic from "../images/dp.jpg"
+import logoutImage from "../images/logout.png"
 
 export default function HomePage() {
   const { data: session } = useSession()
@@ -10,30 +10,32 @@ export default function HomePage() {
   const userName = session!.user.name;
   const userImage = session!.user.image.replaceAll("s96-c", "s192-c");
 
+  var clueNum = 4;
+
   return (
     <>
-      <div id = {styles.home}>
-        <span id = {styles.title}> Queue Our Codes </span><br />
-        <div className = {styles.HomeContainer} id = {styles.UserContainer}>
-          <div id = {styles.UserContainerInner}>
-            <Image id = {styles.userpic} src={userImage} width={32} height={32} alt="Picture of the user"/>
-            <span id = {styles.username}>{userName}</span>
+      <div id={styles.home}>
+        <span id={styles.title}> Queue Our Codes </span><br />
+        <div className={styles.HomeContainer} id={styles.UserContainer}>
+          <div id={styles.UserContainerInner}>
+            <Image id={styles.userpic} src={userImage} width={32} height={32} alt="Picture of the user" />
+            <span id={styles.username}>{userName}</span>
           </div>
-          <Image id = {styles.userpic} src={profilePic} alt="Picture of the user"/>
+          <Image id={styles.logout} src={logoutImage} alt="Picture of the user" onClick={() => signOut()} />
         </div>
-        <div className = {styles.HomeContainer} id = {styles.ClueContainer}>
-            <span id = {styles.cluenum}> Clue #4 </span>
-            <span id = {styles.cluecont}>
+        <div className={styles.HomeContainer} id={styles.ClueContainer}>
+          <span id={styles.cluenum}> Clue #{clueNum} </span>
+          <span id={styles.cluecont}>
             To use me, break me <br /><br />
             I am in front, but you can’t see me <br /><br />
             By the Laws of Physics, what goes up but never goes down? <br /><br />
             I get bigger the more is taken away
-            </span>
+          </span>
         </div>
-        <div className = {styles.HomeContainer}>
-            
+        <div className={styles.ProgressBar}>
+          <div className={styles.Progress} style={{ width: (clueNum / 10) * 100 + "%" }}></div >
         </div>
-        <div className = {styles.Footer}>COMSCI@UP.BAG 2023</div>
+        <div className={styles.Footer}>COMSCI@UP.BAG 2023</div>
       </div>
     </>
   )
