d45c5750329877679dfa74ceb9a53358950ba7ca
Daniel David Bador <danieldavidbador@yahoo.com>
2023-01-26 21:53:37 +0800

    get user name and profile picture

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,13 @@
+import { useSession } from "next-auth/react"
+import Image from "next/image"
 
 export default function HomePage() {
-  return <h1> "Hi there" </h1>
+  const { data: session } = useSession()
+
+  return (
+    <>
+      <h1> "Hi there" {session?.user.name} </h1>
+      <Image src={session!.user.image.replaceAll("s96-c", "s192-c")} alt="Picture of the user" width={200} height={200} />
+    </>
+  )
 }
