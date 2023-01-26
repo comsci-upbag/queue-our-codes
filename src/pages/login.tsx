4306c72623d663fc1726cb60ed39df31fa01049f
Daniel David Bador <danieldavidbador@yahoo.com>
2023-01-27 06:59:28 +0800

    fixed login button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -19,7 +19,7 @@ export async function getServerSideProps(context: NextPageContext) {
   }
 
   return {
-    props: { }
+    props: {}
   }
 }
 
@@ -28,15 +28,15 @@ export default function Login() {
   return (
     <>
       <div className={styles.LoginContainer}>
-          <span id={styles.title}> Queue Our Codes </span><br />
-          <div id={styles.LoginButton} onClick={() => signIn("google")}>
-              Sign in with Google
-          </div>
+        <span id={styles.title}> Queue Our Codes </span><br />
+        <div id={styles.LoginButton} onClick={() => signIn("google")}>
+          Sign in with Google
           <Image src="/googleIcon.svg" id={styles.Google} alt="google-icon" width={25} height={25} />
+        </div>
       </div>
       <div className={styles.Footer}>COMSCI@UP.BAG 2023</div>
       <div className={styles.cover}>
-          <div className={styles.circle}/>
+        <div className={styles.circle} />
       </div>
     </>
   )
