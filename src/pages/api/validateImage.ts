4f0f95647039e31284487788bcb9054a79530824
Daniel David Bador <danieldavidbador@yahoo.com>
2023-02-03 02:29:22 +0800

    todo: validate image

--- src/pages/api/validateImage.ts
+++ src/pages/api/validateImage.ts
@@ -6,14 +6,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { image } = JSON.parse(req.body);
 
   // TODO: convert image to tensor
-  const buffer = Buffer.from(image, 'base64');
-  console.log(image);
+  // const buffer = Buffer.from(image, 'base64');
+  // console.log(image);
 
-  const imageTensor = tf.node.decodeImage(buffer);
+  // const imageTensor = tf.node.decodeImage(buffer);
 
-  console.log(imageTensor);
+  // console.log(imageTensor);
 
 
-  res.status(200).json({message: "hi"});
+  res.status(200).json({ message: "hi" });
 
 }
