e4b68a37df764c817813dbefd458b6e59d82a87c
Daniel David Bador <danieldavidbador@yahoo.com>
2023-02-08 22:54:18 +0800

    updated test answers

--- src/globals/answers.ts
+++ src/globals/answers.ts
@@ -1,43 +1,43 @@
 
 
 export const textAnswers = [
-  { number: 1, answer: "TEST1"},
-  { number: 2, answer: "TEST2"},
-  { number: 3, answer: "TEST3"},
-  { number: 4, answer: "TEST4"},
-  { number: 5, answer: "TEST5"},
-  { number: 6, answer: "TEST6"},
-  { number: 7, answer: "TEST7"},
-  { number: 8, answer: "TEST8"},
-  { number: 9, answer: "TEST9"},
+  { number: 1, answer: "TEST1" },
+  { number: 2, answer: "TEST2" },
+  { number: 3, answer: "TEST3" },
+  { number: 4, answer: "TEST4" },
+  { number: 5, answer: "TEST5" },
+  { number: 6, answer: "TEST6" },
+  { number: 7, answer: "TEST7" },
+  { number: 8, answer: "TEST8" },
+  { number: 9, answer: "TEST9" },
 ]
 
 
 export const imageAnswers = [
-  { number: 1, answer: "TEST1"},
-  { number: 2, answer: "TEST2"},
-  { number: 3, answer: "TEST3"},
-  { number: 4, answer: "TEST4"},
-  { number: 5, answer: "TEST5"},
-  { number: 6, answer: "TEST6"},
-  { number: 7, answer: "TEST7"},
-  { number: 8, answer: "TEST8"},
-  { number: 9, answer: "TEST9"},
+  { number: 1, answer: "TEST1" },
+  { number: 2, answer: "white-cat-yellow-head" },
+  { number: 3, answer: "TEST3" },
+  { number: 4, answer: "TEST4" },
+  { number: 5, answer: "TEST5" },
+  { number: 6, answer: "TEST6" },
+  { number: 7, answer: "TEST7" },
+  { number: 8, answer: "TEST8" },
+  { number: 9, answer: "TEST9" },
 ]
 
 export const qrcodeAnswers = [
-  { number: 1, answer: "TEST1"},
-  { number: 2, answer: "TEST2"},
-  { number: 3, answer: "TEST3"},
-  { number: 4, answer: "TEST4"},
-  { number: 5, answer: "TEST5"},
-  { number: 6, answer: "TEST6"},
-  { number: 7, answer: "TEST7"},
-  { number: 8, answer: "TEST8"},
-  { number: 9, answer: "TEST9"},
+  { number: 1, answer: "TEST1" },
+  { number: 2, answer: "TEST2" },
+  { number: 3, answer: "TEST3" },
+  { number: 4, answer: "TEST4" },
+  { number: 5, answer: "TEST5" },
+  { number: 6, answer: "TEST6" },
+  { number: 7, answer: "TEST7" },
+  { number: 8, answer: "TEST8" },
+  { number: 9, answer: "TEST9" },
 ]
 
-export function isAnswerCorrect(type: "qrcode" | "image" | "text", puzzleId: number, answer: string) : boolean {
+export function isAnswerCorrect(type: "qrcode" | "image" | "text", puzzleId: number, answer: string): boolean {
 
   if (type == "qrcode") {
     const correctAnswer = textAnswers.filter(answer => answer.number == puzzleId)[0].answer;
