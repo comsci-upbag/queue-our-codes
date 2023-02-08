42d53e508f773425a7b918ee4e846235cbbde8f7
Brymer <meneses.brymer@gmail.com>
2023-02-08 21:01:22 +0800

    update participant status when image is correct

--- src/pages/api/validateImage.ts
+++ src/pages/api/validateImage.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from '@prisma/client'
 import { getSession } from "next-auth/react"
+import { isAnswerCorrect } from "@/globals/answers";
 
 const prisma = new PrismaClient();
 
@@ -14,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const session = await getSession({ req });
 
   if (!session) {
-    res.status(404).json({ messages: "No logged in account." });
+    res.status(404).json({ message: "No logged in account." });
     return;
   }
 
@@ -23,5 +24,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const { label, probability } = await result.json() as Prediction;
 
-  res.status(200).json({ label, probability });
+  if (!participant) {
+    res.status(404).json({ message: "Participant is not registered" });
+    return;
+  }
+
+  if (!isAnswerCorrect("image",  participant.current_puzzle,  label)) {
+    res.status(200).json({ isAnswerCorrect: false });
+    return;
+  }
+
+  // update participant data
+  await prisma.participantStatus.update({
+    where: {
+      email: session.user.email,
+    },
+    data: {
+      current_puzzle: participant.current_puzzle + 1,
+    }
+  })
+
+  res.status(200).json({ isAnswerCorrect: true, label, probability });
 }
