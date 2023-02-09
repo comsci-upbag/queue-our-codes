123e0ea2c68716504bd2f44362a2a119ae431684
Daniel David Bador <danieldavidbador@yahoo.com>
2023-02-09 09:26:31 +0800

    changed puzzle 2

--- src/puzzles/Puzzle2.tsx
+++ src/puzzles/Puzzle2.tsx
@@ -1,14 +1,5 @@
-import dynamic from "next/dynamic";
 
-const AlertBox = dynamic(() => import("@/components/AlertBox"));
-
-import Dialogue from "@/components/Dialogue"
-import WebCam from "@/components/WebCam"
-import ConditionalShow from "@/components/ConditionalShow";
-
-import styles from "@/styles/Home.module.css"
-
-import { useState } from "react";
+import styles from "@/styles/Home.module.css";
 
 interface Props {
   puzzleId: number;
@@ -16,47 +7,18 @@ interface Props {
 }
 
 export default function Puzzle2({ puzzleId, currentPuzzle }: Props) {
-  const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null);
-  const [showAlert, setShowAlert] = useState<boolean>(false);
-  const [isDialogueFinished, setIsDialogueFinished] = useState<boolean>(false);
-
-  const validateImage = async (image: string) => {
-    const result = await fetch("api/validateImage", {
-      method: 'POST',
-      body: JSON.stringify({ image })
-    })
-    const { isAnswerCorrect } = await result.json();
-    setIsAnswerCorrect(isAnswerCorrect);
-    setShowAlert(isAnswerCorrect);
-    return result;
-  }
-
   return (
     <>
       <span id={styles.cluecont}>
-        <ConditionalShow shouldShow={!isAnswerCorrect!}>
-          <Dialogue sender="Mr. Cat 1" senderImage="/logo.svg" script={[
-            { type: "send", message: "I want you to look for someone with a white fur and a yellow head." },
-            { type: "reply", message: "Where should I look?" },
-            { type: "send", message: "Why should I tell you?" },
-            { type: "send", message: "It's up to you to find them." },
-            { type: "reply", message: "I see. I'll start looking then!" },
-          ]}
-            isFinished={currentPuzzle !== puzzleId}
-            setIsDialogueFinished={setIsDialogueFinished} />
-        </ConditionalShow>
-        <ConditionalShow shouldShow={isAnswerCorrect!}>
-          <Dialogue sender="Mr. Cat 2" senderImage="/logo.svg" script={[
-            { type: "send", message: "I want you to look for someone with a white fur and a yellow head." },
-            { type: "reply", message: "Where should I look?" },
-            { type: "send", message: "Why should I tell you?" },
-            { type: "send", message: "It's up to you to find them." },
-            { type: "reply", message: "I see. I'll start looking then!" },
-          ]}
-            isFinished={currentPuzzle !== puzzleId} />
-        </ConditionalShow>
-        {currentPuzzle === puzzleId && isDialogueFinished && !isAnswerCorrect && <WebCam buttonLabel="Start Looking" callback={validateImage} />}
-        {showAlert && <AlertBox title={"Congratukations!"} message={"You've found {cat name}!"} type={"success"} show={setShowAlert} />}
+        From this ancient dwelling, our names rest. <br /> <br />
+        Your ancestors has built them <br /> <br />
+        To safekeep their crops from the same pest we hunt. <br /> <br />
+        <br /> <br />
+        Let our presence guide you to your lofty quest; <br /> <br />
+        'Cause I know one of us had a delightful feast. <br /> <br />
+        <br /> <br />
+        Remember, we are not one, but among many you will see. <br /> <br />
+        <div style={{ textAlign: "right", width: "100%" }}>- The Fale Sleeper </div>
       </span>
     </>
   )
