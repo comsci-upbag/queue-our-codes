cb6fa576b1d74dbda5906a05a628613ca0cb4b32
Daniel David Bador <danieldavidbador@yahoo.com>
2023-02-09 09:46:02 +0800

    fixed answers

--- src/globals/answers.ts
+++ src/globals/answers.ts
@@ -15,11 +15,11 @@ export const textAnswers = [
 
 export const imageAnswers = [
   { number: 1, answer: "TEST1" },
-  { number: 2, answer: "white-cat-yellow-head" },
+  { number: 2, answer: "TEST2" },
   { number: 3, answer: "TEST3" },
   { number: 4, answer: "TEST4" },
   { number: 5, answer: "TEST5" },
-  { number: 6, answer: "TEST6" },
+  { number: 6, answer: "white-cat-yellow-head" },
   { number: 7, answer: "TEST7" },
   { number: 8, answer: "TEST8" },
   { number: 9, answer: "TEST9" },
@@ -40,7 +40,7 @@ export const qrcodeAnswers = [
 export function isAnswerCorrect(type: "qrcode" | "image" | "text", puzzleId: number, answer: string): boolean {
 
   if (type == "qrcode") {
-    const correctAnswer = textAnswers.filter(answer => answer.number == puzzleId)[0].answer;
+    const correctAnswer = qrcodeAnswers.filter(answer => answer.number == puzzleId)[0].answer;
     return correctAnswer === answer;
   }
 
