dbc4f0fca1ba6b3bd3ef70066683a7d31c45874d
Daniel David Bador <danieldavidbador@yahoo.com>
2023-02-12 12:00:50 +0800

    updated puzzle 2

--- src/puzzles/Puzzle2.tsx
+++ src/puzzles/Puzzle2.tsx
@@ -22,30 +22,24 @@ export default function Puzzle2({ puzzleId, currentPuzzle }: Props) {
           since this is a very urgent matter, you decided to wake them up. 
           They seemed very grumpy because of what you did."
           type="narration"
-          callback={setShowDialogue}/>
+          callback={setShowDialogue} />
       </div>
 
       <Show when={showDialogue}>
         <Dialogue sender="Mr. Cat" senderImage="/logo.svg" isFinished={currentPuzzle !== puzzleId} script={[
-                    { type: "send", message: "Why are you here? *meow*" },
-                    { type: "send", message: "Why are people disturbing my sleep? *meow*" }, 
-                    { type: "reply", message: "Hello, can I ask why you were around the (PLACE)?" }, 
-                    { type: "reply", message: "What were you doing there?" }, 
-                    { type: "send", message: "Why do you care? *meow* I go everywhere, that place is nothing special to me."},
-                    { type: "reply", message: "I am investigating what happened in that place. Right now, you’re the only cat we know who has been there."},
-                    { type: "send", message: "Meowkay. *meow* How do I prove my innocence?" },
-                    { type: "send", message: "I’m not going to tell you other cats’ secrets, but I know some shady cat who might. *meow* You can reach them through this note:" },
-                  ]}
-                  setIsDialogueFinished={setIsDialogueFinished}
+          { type: "send", message: "Why are you here? *meow*" },
+          { type: "send", message: "Why are people disturbing my sleep? *meow*" },
+          { type: "reply", message: "Hello, can I ask why you were around the (PLACE)?" },
+          { type: "reply", message: "What were you doing there?" },
+          { type: "send", message: "Why do you care? *meow* I go everywhere, that place is nothing special to me." },
+          { type: "reply", message: "I am investigating what happened in that place. Right now, you’re the only cat we know who has been there." },
+          { type: "send", message: "Meowkay. *meow* How do I prove my innocence?" },
+          { type: "send", message: "I’m not going to tell you other cats’ secrets, but I know some shady cat who might. *meow* You can reach them through this note:" },
+          { type: "send", message: "EDGAR ALLAN POE knows of a SUPERSTITION about BALL SPORTS." },
+        ]}
+          setIsDialogueFinished={setIsDialogueFinished}
         />
       </Show>
-
-        <Show when={isDialogueFinished}>
-          <div className={styles.narration}>
-            EDGAR ALLAN POE knows of a SUPERSTITION about BALL SPORTS.
-          </div>
-        </Show>
-
     </>
   )
 }
