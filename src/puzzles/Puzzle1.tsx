5844c318aa8ebc311cd3c52fe7142198675fbc34
Daniel David Bador <danieldavidbador@yahoo.com>
2023-02-12 09:55:11 +0800

    updated puzzle 1

--- src/puzzles/Puzzle1.tsx
+++ src/puzzles/Puzzle1.tsx
@@ -8,6 +8,8 @@ import styles from "@/styles/Puzzle.module.css"
 
 import { useState } from "react";
 
+import { TypeAnimation } from "react-type-animation";
+
 interface Props {
   puzzleId: number;
   currentPuzzle: number;
@@ -16,6 +18,8 @@ interface Props {
 export default function Puzzle1({ puzzleId, currentPuzzle }: Props) {
   const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null);
   const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [showInstructions, setShowInstructions] = useState<boolean>(false);
+  const [showWebcam, setShowWebcam] = useState<boolean>(false);
 
   const validateImage = async (image: string) => {
     const result = await fetch("api/validateImage", {
@@ -38,13 +42,24 @@ export default function Puzzle1({ puzzleId, currentPuzzle }: Props) {
     <>
       <div className={styles.container}>
         <div className={styles.narration}>
-          While searching for this place, you found something odd — a cream-colored fur. Delighted, you have decided to ask the cats their testimonies and alibis.
+          <TypeAnimation
+            sequence={[0, "While searching for this place, you found something odd — a cream-colored fur. Delighted, you have decided to ask the cats their testimonies and alibis.", () => {
+              setShowInstructions(true);
+            }]}
+            cursor={false}
+            speed={75}
+            wrapper="div" />
         </div>
         <div className={styles.direction}>
-          Use the button below to scan the picture of the cat. This will run an AI model to validate that the cat you are investigating is correct. While scanning a cat, make sure that no other cats are in view of your camera.
+          {showInstructions && <TypeAnimation sequence={[0, "Use the button below to scan the picture of the cat. This will run an AI model to validate that the cat you are investigating is correct. While scanning a cat, make sure that no other cats are in view of your camera.", () => {
+            setShowWebcam(true)
+          }]}
+            cursor={false}
+            speed={75}
+            wrapper="div" />}
         </div>
 
-        <WebCam buttonLabel="Start Looking" callback={validateImage} />
+        {showWebcam && <WebCam buttonLabel="Start Looking" callback={validateImage} />}
 
         <AlertBox showWhen={showAlert && !isAnswerCorrect} title="Wrong answer!" message="Sadly, this is not the cat we are looking for." type="warning" show={setShowAlert} />
         <AlertBox showWhen={showAlert && isAnswerCorrect} title={"Congratulations!"} message={"You've found Tonton!"} type={"success"} show={setShowAlert} />
