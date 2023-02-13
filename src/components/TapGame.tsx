1c626864356bae6e5623360a9c92f904418e1eb3
Brymer <meneses.brymer@gmail.com>
2023-02-13 11:15:16 +0800

    create TapGame component

--- src/components/TapGame.tsx
+++ src/components/TapGame.tsx
@@ -1,6 +1,34 @@
+
+import { useDebounce } from "@/globals/hooks"
+import { useEffect, useState } from "react"
+
 export default function TapGame() {
-  return (
-    <>
-    </>
-  )
+
+  const [numClicked, setNumClicked] = useState(0);
+  const debouncedValue = useDebounce(numClicked, 1000);
+
+  const onClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setNumClicked(numClicked + 1);
+  }
+
+  useEffect(() => {
+
+    const request = { answer: debouncedValue }
+
+    fetch("api/validateAnswer", {
+      method: "POST",
+      body: JSON.stringify(request),
+    }).then((data) => data.json())
+      .then((data) => {
+        // if (data.isAnswerCorrect) {
+        //   // do something
+        // } else {
+        //   // do something
+        // }
+      })
+
+  }, [debouncedValue])
+
+  return <button onClick={onClick}> 🐱 </button>
 }
