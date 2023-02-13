338d5c0815b2814cc797b87392489d2dfe26aa91
Daniel David Bador <danieldavidbador@yahoo.com>
2023-02-13 11:32:32 +0800

    added crossword component

--- src/components/Crossword.tsx
+++ src/components/Crossword.tsx
@@ -1,24 +1,71 @@
-import styles from "@/styles/Crossword.module.css"
+import { answerBoxVisibilityState } from "@/globals/states";
 
 import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import ReactCrossword from "@jaredreisinger/react-crossword"
 
-const puzzle = [
-  ['█', '█', '█', '█', '█', '█',],
-  ['█', '█', '█', '█', '█', '█',],
-]
+import styles from "@/styles/Crossword.module.css"
 
+const data = {
+  across: {
+    1: {
+      clue: 'Solve the other puzzles to find me.',
+      answer: 'SIKE',
+      row: 7,
+      col: 5,
+    }
+  },
+  down: {
+    1: {
+      clue: 'I disappear as you say my name.',
+      answer: 'SILENCE',
+      row: 7,
+      col: 5,
+    },
+    2: {
+      clue: 'Iron + ⎯⎯⎯',
+      answer: 'FELINE',
+      row: 4,
+      col: 6,
+    },
+    3: {
+      clue: 'They eat me, then they get eaten.',
+      answer: 'FISHHOOK',
+      row: 0,
+      col: 7,
+    },
+    4: {
+      clue: 'Cats, well even dogs, chase me even though they can’t touch me.',
+      answer: 'LASER',
+      row: 4,
+      col: 8,
+    },
+  },
+};
 
 export default function Crossword() {
   const [startGame, setStartGame] = useState(false);
+  const setShowAnswerBox = useSetRecoilState(answerBoxVisibilityState)
+
   return (
     <>
       <div className={styles.container}>
         {startGame ?
           <>
+            <div style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyItems: "center",
+              alignItems: "center"
+            }}>
+              <div style={{ width: "fit-content" }}>
+                <ReactCrossword data={data} onCrosswordCorrect={() => setShowAnswerBox(true)} theme={{ highlightBackground: 'var(--shadow-color1)' }} />
+              </div>
+            </div>
           </>
           :
           <>
-            <button className={styles.StartButton} onClick={() => setStartGame(true)}>Start Crossword</button>
+            <button onClick={() => setStartGame(true)}>Start Crossword</button>
           </>
         }
       </div>
